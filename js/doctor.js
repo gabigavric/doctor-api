c15d563264eb31a81a88960a0c4a8190b27144c2
gabrijela <gabigavric@gmail.com>
2017-10-20 14:07:50 -0700

    used postman to modefy api key

--- js/doctor.js
+++ js/doctor.js
@@ -3,7 +3,7 @@ export class Doctor {
   callApi()
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = "https://api.betterdoctor.com/2016-03-01/doctors?query=toothache&location=45.5231%C2%B0%20N%2C%20122.6765%C2%B0%20W&user_location=37.773%2C-122.413&skip=0&limit=25"
+      let url = "https://api.betterdoctor.com/2016-03-01/doctors?query=toothache&location=97219&skip=0&limit=25&user_key`${apiKey}`"
 
      request.onload = function() {
        if (this.status === 200) {
