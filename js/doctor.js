704665866d9b8f9cb4407c759c68a83a0e44d191
gabrijela <gabigavric@gmail.com>
2017-10-26 11:30:38 -0700

    added curley brackets and replaced name of callApi to makePromise and passed in apikey

--- js/doctor.js
+++ js/doctor.js
@@ -1,14 +1,16 @@
-const apiKey = require('./../.env').apiKey;
+// const apiKey = require('./../.env').apiKey; DO I NEED THIS?
+
+//ALSO WHERE DO I PUT THIS
+//`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&location=97219&skip=0&limit=10&user_key=${apiKey}`;
 
 export class Doctor {
   constructor() { ////close this
 }
 
-callApi(query)
+makePromise(api_key){
   let promise = new Promise((resolve, reject) => {
     let request = new XMLHttpRequest();
-    let url =
-    `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&location=97219&skip=0&limit=10&user_key=${apiKey}`;
+    let url = api_key
     request.onload = () => {
       if (request.status === 200) {
         resolve(request.response);
@@ -19,3 +21,4 @@ callApi(query)
     request.open("GET", url, true);
     request.send();
   });
+}
