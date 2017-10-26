86c0ecad4b7ba42af8aa3bf78e74622410588236
gabrijela <gabigavric@gmail.com>
2017-10-26 11:32:55 -0700

    modify interface file to include the promise variable which holds my api url

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,10 +2,15 @@ import { Doctor } from './../js/doctor.js';
 let apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
-  $("#form").submit(function() {
+
+  $('#form').submit(function(event) {
     event.preventDefault();
-    $('#showResults').text('');
-    let query = $('#query').val();
+    let doctor = new Doctor();
+    let concern = $('#concern').val();
+    let name = $('#name').val();
+    //do i define promise in here and put the link in here?
+    let promise = doctor.makePromise(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${concern}&location=97219&skip=0&limit=10&user_key=${apiKey}`;)
     doctor.callApi(query);
   });
+
 });
