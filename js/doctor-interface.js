c885cb625e6213318cf157f8eb3bb8e142eaa1a5
gabrijela <gabigavric@gmail.com>
2017-10-28 17:15:33 -0700

    added state zip phone address patients

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -14,9 +14,19 @@ console.info("document is ready");
     promise.then(function(response) {
       let data = JSON.parse(response);
       console.info(data);
-      let practices = data.data[0].practices;
-      if(practices.length > 0){
-          $('#practices').text(`${practices[0].name}`);
+      let practices = data.data[0].practices[0];
+      let profile = data.data[0].profile;
+      let address =practices.visit_address;
+      console.info(profile);
+      if(data.meta.count > 0){
+          $('#practices').text(`${profile.first_name} ${profile.last_name}`+
+          ` ${practices.phones[0].number}` +
+          ` ${address.street}` +
+          ` ${address.state_long}`+
+          ` ${address.city}`+
+          ` ${address.zip}` +
+          ` ${practices.website}`+
+          ` Accepting New Patients: ${practices.accepts_new_patients}`);
       }else{
           $('#practices').text("No Doctors Found");
       }
